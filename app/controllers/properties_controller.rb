f46cfc43ca94efc00363a83ddfbf187e065a71a9
Vasilios Kambouras <Vasilioskw@gmail.com>
2021-07-09 23:58:11 -0400

    account id error fixed by setting it to current account

--- app/controllers/properties_controller.rb
+++ app/controllers/properties_controller.rb
@@ -22,6 +22,7 @@ class PropertiesController < ApplicationController
   # POST /properties or /properties.json
   def create
     @property = Property.new(property_params)
+    @property.accout_id = current_account.id
 
     respond_to do |format|
       if @property.save
