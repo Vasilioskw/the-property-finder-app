5a7ce62fb8d29a6100bd8eafd00814dbfd6517eb
Vasilios Kambouras <Vasilioskw@gmail.com>
2021-07-13 12:36:53 -0400

    added a field to allow addition of image files in property post

--- app/controllers/properties_controller.rb
+++ app/controllers/properties_controller.rb
@@ -66,6 +66,6 @@ class PropertiesController < ApplicationController
 
     # Only allow a list of trusted parameters through.
     def property_params
-      params.require(:property).permit(:name, :price, :rooms, :bathrooms)
+      params.require(:property).permit(:name, :price, :rooms, :bathrooms, :photo, :photo_cashe)
     end
 end
