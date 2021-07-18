60e526e986429488dbd6c7e998c9eb8b3c1c45d0
Vasilios Kambouras <Vasilioskw@gmail.com>
2021-07-18 10:03:33 -0400

    Attempting photo resize using minimagic. Will make another branch to try with carrierwave

--- app/models/property.rb
+++ app/models/property.rb
@@ -1,7 +1,7 @@
 class Property < ApplicationRecord
     # mount_uploader :photo, PhotoUploader
     has_one_attached :photo
-    # has_one_attached :photo_cashe
+    # has_one_attached :photo_cache
     belongs_to :account
 
     scope :latest, -> { order created_at: :desc }
