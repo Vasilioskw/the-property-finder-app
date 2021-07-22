f422778470563390cc567115393c7e0a3303818e
Vasilios Kambouras <Vasilioskw@gmail.com>
2021-07-22 00:59:12 -0400

    added details attribute to schema -> properties. Now able to do CRUD operations for it

--- app/controllers/properties_controller.rb
+++ app/controllers/properties_controller.rb
@@ -72,7 +72,7 @@ class PropertiesController < ApplicationController
 
     # Only allow a list of trusted parameters through.
     def property_params
-      params.require(:property).permit(:name, :address, :price, :rooms, :bathrooms, :parking_spaces, :photo)
+      params.require(:property).permit(:name, :address, :price, :rooms, :bathrooms, :parking_spaces, :details, :photo)
     end
 
    
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_07_21_150029) do
+ActiveRecord::Schema.define(version: 2021_07_22_042958) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -84,6 +84,7 @@ ActiveRecord::Schema.define(version: 2021_07_21_150029) do
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
     t.integer "parking_spaces", default: 0
+    t.text "details"
     t.index ["account_id"], name: "index_properties_on_account_id"
   end
 
