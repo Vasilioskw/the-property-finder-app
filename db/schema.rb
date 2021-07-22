b6af2b040e72720bd7babe4d2df1cd9c2e4dc81b
Vasilios Kambouras <Vasilioskw@gmail.com>
2021-07-21 21:15:39 -0400

    adding parking spaces to db/schema and form as a property attribute

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_07_17_120423) do
+ActiveRecord::Schema.define(version: 2021_07_21_150029) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -83,6 +83,7 @@ ActiveRecord::Schema.define(version: 2021_07_17_120423) do
     t.string "photo"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.integer "parking_spaces", default: 0
     t.index ["account_id"], name: "index_properties_on_account_id"
   end
 
