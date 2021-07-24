2cf1e2b9138078d23102a05186074c3d46d187e3
Vasilios Kambouras <Vasilioskw@gmail.com>
2021-07-24 14:18:43 -0400

    congifuring jquery

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -2,23 +2,25 @@
 // present in this directory. You're encouraged to place your actual application logic in
 // a relevant structure within app/javascript and only use these pack files to reference
 // that code so it'll be compiled.
-require("@rails/ujs").start();
-
-
-const webpack = require('webpack')
-environment.plugins.prepend('Provide', 
-    new webpack.ProvidePlugin({
-         $: 'jquery/src/jquery',
-        jQuery: 'jquery/src/jquery',
-        popper: ['popper.js', 'default']
-    })
-)
-
-require("bootstrap");
-require("./includes/properties");
-
 import Rails from "@rails/ujs"
 import * as ActiveStorage from "@rails/activestorage"
+
+
+
+require("@rails/ujs").start()
+
+require("jquery");
+require("./includes/properties");
+
+var jQuery = require("jquery")
+global.$ = global.jQuery = jQuery
+window.$ = window.jQuery = jQuery
+
+require("bootstrap");
+
+require(".includes/properties");
+
+
 
 Rails.start()
 ActiveStorage.start()
