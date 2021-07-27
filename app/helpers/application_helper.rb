233ad8d1d9a76a2edaa732cfe1219fe43f5e08a1
Vasilios Kambouras <Vasilioskw@gmail.com>
2021-07-26 22:13:48 -0400

    defining a profile image helper methode to make account image round and ready for render

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,8 @@
 module ApplicationHelper
+
+    def profile_picture account
+        thumb = account.image,present? ? account.image.thumb.url : "placeholder.jpeg"
+        image_tag thumb, class: "profile_pic img-circle"
+
+    end
 end
