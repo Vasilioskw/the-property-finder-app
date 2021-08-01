7efc92cd57cbcebad620de0a9c0a9c82b45899f5
Vasilios Kambouras <Vasilioskw@gmail.com>
2021-08-01 14:13:51 +0300

    PROFILE PICTURES ARE SUCCESSFULLY RENDERING

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -2,10 +2,8 @@ module ApplicationHelper
 
     def profile_picture account
 
-        img = account.image.present? ? account.image : "placeholder.png"
-        image_tag img, class: "profile_pic img-circle" 
-        # thumb = account.image.present? ? account.image.thumb.url : "placeholder.jpeg"
-        # image_tag thumb, class: "profile_pic img-circle"
+        img = property.image.present? ? property.image : "placeholder.png"
+        image_tag img, class: "property-thumb" 
 
     end
 end
