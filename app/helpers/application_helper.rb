89572d7cd7b1262da7f97ebc94e0e45d5918a0e3
Vasilios Kambouras <Vasilioskw@gmail.com>
2021-08-03 22:29:32 +0300

    resized agent profile pic to look more presentable in relation to size

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -3,7 +3,7 @@ module ApplicationHelper
     def profile_picture account
 
         img = property.image.present? ? property.image : "placeholder.png"
-        image_tag img, class: "property-thumb" 
-
+        image_tag img, class: "profile-pic img-circle" 
+ 
     end
 end
