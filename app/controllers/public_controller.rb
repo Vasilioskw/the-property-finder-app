44412184ae3641cec2e13f3cf99fb36da17d1be4
Vasilios Kambouras <Vasilioskw@gmail.com>
2021-08-08 13:28:24 +0300

    added toaster for notifactions

--- app/controllers/public_controller.rb
+++ app/controllers/public_controller.rb
@@ -1,5 +1,9 @@
 class PublicController < ApplicationController
   def main
+    if account_signed_in?
+      redirect_to dashboard_path, flash: { success: "Successfully signed in. Welcome to Property Finder!" } and return
+    end
+
     @properties = Property.latest
   end
 end
