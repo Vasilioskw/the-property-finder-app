ff0f2698e0865324d44415d9096f6db0585f66dd
Vasilios Kambouras <Vasilioskw@gmail.com>
2021-09-08 22:57:10 -0400

    added telephone migration for agent info

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_08_11_120353) do
+ActiveRecord::Schema.define(version: 2021_09_08_190700) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -37,6 +37,7 @@ ActiveRecord::Schema.define(version: 2021_08_11_120353) do
     t.datetime "updated_at", precision: 6, null: false
     t.string "image"
     t.string "company"
+    t.string "telephone"
     t.index ["email"], name: "index_accounts_on_email", unique: true
     t.index ["reset_password_token"], name: "index_accounts_on_reset_password_token", unique: true
   end
